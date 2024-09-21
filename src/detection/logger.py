b346d8e83dd26ed8ce11800cc39c3788237b2a9f
David Tarca <dt764@inlumine.ual.es>
2024-09-21 14:18:23 +0200

    Actualizado logger para que guarde la foto con la matricula en el nombre

--- src/detection/logger.py
+++ src/detection/logger.py
@@ -4,7 +4,7 @@ import datetime
 import cv2
 
 class Logger:
-    def __init__(self, img_dir ,csv_file='detections.csv'):
+    def __init__(self, img_dir ,csv_file):
         self.csv_file = csv_file
         self.img_dir = img_dir
         os.makedirs(self.img_dir, exist_ok=True)
@@ -14,10 +14,14 @@ class Logger:
                 writer = csv.writer(file)
                 writer.writerow(['Timestamp', 'License Plate', 'Image Path'])
 
-    def save_detection_to_csv(self, timestamp, plate, image_path):
+    def save_detection_to_csv(self, detected_plate, timestamp, frame):
         with open(self.csv_file, mode='a', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow([timestamp, plate, image_path])
 
-    def save_image(self, frame, path):
-        cv2.imwrite(path, frame)
+            writer = csv.writer(file)
+
+            timestamp_str = timestamp.strftime('%Y%m%d_%H%M%S')
+            image_path = self.img_dir / f'{timestamp_str}_{detected_plate}.jpg'
+
+            writer.writerow([timestamp_str, detected_plate, image_path])
+
+            cv2.imwrite(image_path, frame)
