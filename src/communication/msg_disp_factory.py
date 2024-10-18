ff3b50b919e85bf291faa0310e230401f8611ad4
David Tarca <dt764@inlumine.ual.es>
2024-10-18 19:38:03 +0200

    Añadida documentacion a las clases de mensajeria.

--- src/communication/msg_disp_factory.py
+++ src/communication/msg_disp_factory.py
@@ -4,11 +4,30 @@ from .msg_dispatcher import MsgDispatcher
 from .handlers import *
 
 class MsgDispatcherFactory:
-    # Definir los nombres de las colas como constantes
-    VERIFIER_QUEUE_NAME = 'verifier_queue'
-    DETECTOR_QUEUE_NAME = 'detector_queue'
-    EXCHANGE_NAME = 'detection_exchange'
+ 
+    """
+
+    This class if for creating the MsgDispatcher objects necessary
+    for each program. Having this factory ensure consitency with the
+    queue / exchange names, and their types.
+
+    """
+
+    VERIFIER_QUEUE_NAME = 'verifier_queue' # The queue between the parking main and the verifier
+    DETECTOR_QUEUE_NAME = 'detector_queue' # The queue which the parking main will use to receive messages from the gate main.
+    EXCHANGE_NAME = 'detection_exchange' # The exchange which will receive the verification results.
     
+
+    """
+    Creates the MsgDispatcher object for the parking main.
+
+    Args:
+        hostname (String): The URL / domain name of the message broker.
+
+    Returns:
+        The MsgDispatcher object for the parking main.
+    """
+
     @staticmethod
     def create_detector_dispatcher(hostname):
         return MsgDispatcher(
@@ -21,6 +40,18 @@ class MsgDispatcherFactory:
             reply_to_received_message=False,
             stop_consumimg_after_received_message=True
         )
+    
+    """
+
+    Creates the MsgDispatcher object for the verifier main.
+
+    Args:
+        hostname (String): The URL / domain name of the message broker.
+
+    Returns:
+        The MsgDispatcher object for the verifier main.
+
+    """
 
     @staticmethod
     def create_verifier_dispatcher(hostname):
@@ -34,6 +65,18 @@ class MsgDispatcherFactory:
             reply_to_received_message=True,
             stop_consumimg_after_received_message=False
         )
+    
+    """
+
+    Creates the MsgDispatcher object for the screen main.
+
+    Args:
+        hostname (String): The URL / domain name of the message broker.
+
+    Returns:
+        The MsgDispatcher object for the screen main.
+
+    """
 
     @staticmethod
     def create_screen_dispatcher(hostname):
@@ -47,6 +90,18 @@ class MsgDispatcherFactory:
             reply_to_received_message=False,
             stop_consumimg_after_received_message=False
         )
+    
+    """
+
+    Creates the MsgDispatcher object for the gate main.
+
+    Args:
+        hostname (String): The URL / domain name of the message broker.
+
+    Returns:
+        The MsgDispatcher object for the gate main.
+
+    """
 
     @staticmethod
     def create_gate_dispatcher(hostname):
--- src/communication/msg_dispatcher.py
+++ src/communication/msg_dispatcher.py
@@ -2,6 +2,39 @@ import pika
 from pika.exchange_type import ExchangeType
 
 class MsgDispatcher:
+
+    """
+    Class to manage communication with RabbitMQ, both for sending and receiving messages.
+
+    It allows setting up queues or fanout exchanges and handling received messages with a
+    custom handler. It can also be configured to reply to received messages and stop consuming 
+    after receiving a message.
+
+    Args:
+        hostname (str): The RabbitMQ server's hostname or IP address.
+        publish_queue_name (str): The name of the queue or exchange for publishing messages.
+        publish_is_fanout (bool): If True, a fanout exchange is set up for publishing.
+        receive_queue_name (str): The name of the queue or exchange for receiving messages.
+        receive_is_fanout (bool): If True, a fanout exchange is set up for receiving.
+        msg_handler (callable): Function to handle received messages.
+        stop_consumimg_after_received_message (bool): If True, stops consuming (or waiting for messages) after receiving a message. This is used when some other processing needs to be done before waiting for another message
+        reply_to_received_message (bool): If True, replies to the received message.
+
+    Attributes:
+        connection (pika.BlockingConnection): Connection to RabbitMQ.
+        channel (pika.BlockingChannel): Channel for communicating with RabbitMQ.
+        publish_queue_name (str): Name of the queue or exchange for publishing messages.
+        receive_queue_name (str): Name of the queue or exchange for receiving messages.
+        publish_is_fanout (bool): If True, the publish exchange is a fanout type.
+        receive_is_fanout (bool): If True, the receive exchange is a fanout type.
+        queue (pika.spec.Queue): Queue for receiving messages when using fanout exchange.
+        msg_handler (callable): Function to handle received messages.
+        stop_consuming_after_received_msg (bool): If True, stops consuming (or waiting for messages) after receiving a message.  
+        reply_to_received_msg (bool): If True, replies to the received message.
+        last_reply_result (Any): The last result returned by the msg_handler function.
+    """
+
+
     def __init__(
             self, hostname,
             publish_queue_name, publish_is_fanout,
@@ -10,26 +43,34 @@ class MsgDispatcher:
             reply_to_received_message
         ):
         
-        # Establecer conexión con RabbitMQ una vez
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(hostname)
         )
         
         self.channel = self.connection.channel()
 
+        # This could be the name of the exchange, if it is of type fanout
         self.publish_queue_name = publish_queue_name
         self.receive_queue_name = receive_queue_name
 
+        # It is needed to know if the exchange / queue is fanaout,
+        # since the way to declare and create them is different from one another
         self.publish_is_fanout = publish_is_fanout
         self.receive_is_fanout = receive_is_fanout
 
         if(publish_is_fanout):
+            # When its fanout, you need to declare the exchange, with the specified name.
             self.channel.exchange_declare(
                 exchange=self.publish_queue_name, exchange_type=ExchangeType.fanout)
         else:
+            # When its not, a queue is created
             self.channel.queue_declare(queue=self.publish_queue_name)
 
+
+        # Its the same thing for the receive exchange / queue.
         if(receive_is_fanout):
+
+            # When its fanout, you declare the exchange and bind a temporary queue to the exchange.
 
             self.channel.exchange_declare(
                 exchange=self.receive_queue_name, exchange_type=ExchangeType.fanout)
@@ -43,12 +84,16 @@ class MsgDispatcher:
                 queue=self.queue.method.queue,
                 on_message_callback=self._on_message_received)
         else:
+
+            #If not, a queue is declared with the specified name.
             self.channel.queue_declare(queue=self.receive_queue_name)
             self.channel.basic_consume(
                 queue=self.receive_queue_name,
                 on_message_callback=self._on_message_received)
 
         self.msg_handler = msg_handler
+
+        # This is used
         self.stop_consuming_after_received_msg = stop_consumimg_after_received_message
         self.reply_to_received_msg = reply_to_received_message
 
@@ -56,6 +101,13 @@ class MsgDispatcher:
 
 
     def send_msg(self, message):
+        """
+        Sends a message to the configured queue or exchange.
+
+        Args:
+            message (str): The message to be sent.
+        """
+        
         if(self.publish_is_fanout):
             self.channel.basic_publish(
                 exchange=self.publish_queue_name, routing_key='', body=message)
@@ -68,12 +120,30 @@ class MsgDispatcher:
             self, ch, method, properties, body,
             ):
         
+        """
+        Handles the receipt of a message.
+
+        Args:
+            ch (BlockingChannel): The communication channel.
+            method (pika.spec.Basic.Deliver): Delivery method.
+            properties (pika.spec.BasicProperties): Message properties.
+            body (bytes): The body of the received message.
+        """
+        
         print(f"Mensaje recibido: {body}")
         
+
         if self.stop_consuming_after_received_msg:
-            ch.stop_consuming()  # Detener el consumo tras recibir la respuesta
+            # This is needed when the program has to do something else after handling the message.
+            # This is the case for the parking main, where it needs to do more more work after receiving and processing the message.
+            # Another solution would be to create another thread for this, but this solution hasnt been studied yet.
+            ch.stop_consuming() 
 
         ch.basic_ack(delivery_tag=method.delivery_tag)
+
+        # Every program that uses this type of object may have a different way of handling messages.
+        # E.g. The parking waits and handles a different message compared to the gate program.
+        # The result is assigned to the atribute, to get it later, and more work with it.
 
         self.last_reply_result = self.msg_handler(body)
 
@@ -82,12 +152,29 @@ class MsgDispatcher:
 
     
     def get_reply_result(self):
+        """
+        Retrieves the last result returned by the message handler. This is used when the MsgDispatcher stops consuming,
+        to do more processing with the handled message, beforw waiting again for more messages.
+
+        Returns:
+            Any: The result from the last execution of the msg_handler.
+        """
         return self.last_reply_result
         
 
     def wait_and_receive_msg(self):
+        """
+
+        Starts wating to receive messages.
+
+        """
         self.channel.start_consuming()
 
     def close(self):
+        """
+
+        Closes the connection to RabbitMQ.
+
+        """
         if self.connection:
             self.connection.close()
