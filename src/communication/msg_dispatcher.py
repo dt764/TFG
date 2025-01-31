6394d4db18f6e3ec5d6a98e1cd9cd9790bdc74ad
David Tarca <dt764@inlumine.ual.es>
2025-01-31 13:14:02 +0100

    Añadido reintentos en caso de errores de conexión.

--- src/communication/msg_dispatcher.py
+++ src/communication/msg_dispatcher.py
@@ -1,5 +1,8 @@
 import pika
+import logging
 from pika.exchange_type import ExchangeType
+import time
+
 
 class MsgDispatcher:
 
@@ -42,56 +45,6 @@ class MsgDispatcher:
             msg_handler, stop_consuming_after_received_message,
             reply_to_received_message
         ):
-        
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(hostname)
-        )
-        
-        self.channel = self.connection.channel()
-
-        # This could be the name of the exchange, if it is of type fanout
-        self.publish_queue_name = publish_queue_name
-        self.receive_queue_name = receive_queue_name
-
-        # It is needed to know if the exchange / queue is fanaout,
-        # since the way to declare and create them is different from one another
-        self.publish_is_fanout = publish_is_fanout
-        self.receive_is_fanout = receive_is_fanout
-
-        if(self.publish_queue_name is not None):
-            if(publish_is_fanout):
-                # When its fanout, you need to declare the exchange, with the specified name.
-                self.channel.exchange_declare(
-                    exchange=self.publish_queue_name, exchange_type=ExchangeType.fanout)
-            else:
-                # When its not, a queue is created
-                self.channel.queue_declare(queue=self.publish_queue_name)
-
-
-        # Its the same thing for the receive exchange / queue.
-        if(self.receive_queue_name is not None):
-            if(receive_is_fanout):
-
-                # When its fanout, you declare the exchange and bind a temporary queue to the exchange.
-
-                self.channel.exchange_declare(
-                    exchange=self.receive_queue_name, exchange_type=ExchangeType.fanout)
-        
-                self.queue = self.channel.queue_declare(queue='', exclusive=True)
-
-                self.channel.queue_bind(
-                    exchange=self.receive_queue_name, queue=self.queue.method.queue)
-                
-                self.channel.basic_consume(
-                    queue=self.queue.method.queue,
-                    on_message_callback=self._on_message_received)
-            else:
-
-                #If not, a queue is declared with the specified name.
-                self.channel.queue_declare(queue=self.receive_queue_name)
-                self.channel.basic_consume(
-                    queue=self.receive_queue_name,
-                    on_message_callback=self._on_message_received)
 
         self.msg_handler = msg_handler
 
@@ -100,31 +53,122 @@ class MsgDispatcher:
         self.reply_to_received_msg = reply_to_received_message
 
         self.last_reply_result = None
+        self.hostname = hostname
+
+         # This could be the name of the exchange, if it is of type fanout
+        self.publish_queue_name = publish_queue_name
+        self.receive_queue_name = receive_queue_name
+
+        # It is needed to know if the exchange / queue is fanaout,
+        # since the way to declare and create them is different from one another
+        self.publish_is_fanout = publish_is_fanout
+        self.receive_is_fanout = receive_is_fanout
+        
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.info("Iniciando MsgDispatcher...")
+
+        # Establish connection
+        self.__reconnect()
+
+
+    def __reconnect(self):
+        max_retries = 5
+        retry_delay = 2
+        attempt = 0
+        while attempt < max_retries:
+            try:
+                self.connection = pika.BlockingConnection(
+                    pika.ConnectionParameters(
+                        self.hostname,
+                        heartbeat=600,
+                        blocked_connection_timeout=300
+                    )
+                )
+                self.channel = self.connection.channel()
+                self.logger.info("Conexión a RabbitMQ establecida con éxito.")
+                self.__setup_queues()
+                return
+            except pika.exceptions.AMQPConnectionError as e:
+                attempt += 1
+                self.logger.warning(f"Error al intentar reconectar con RabbitMQ: {e}. Intento {attempt} de {max_retries}.")
+                time.sleep(retry_delay)
+        self.logger.error("No se pudo establecer la conexión después de varios intentos.")
+        raise Exception("No se pudo conectar a RabbitMQ después de varios intentos")
+
+    def __setup_queues(self):
+        if self.publish_queue_name:
+            if self.publish_is_fanout:
+                self.channel.exchange_declare(
+                    exchange=self.publish_queue_name, exchange_type=ExchangeType.fanout
+                )
+            else:
+                self.channel.queue_declare(queue=self.publish_queue_name)
+        if self.receive_queue_name:
+            if self.receive_is_fanout:
+                self.channel.exchange_declare(
+                    exchange=self.receive_queue_name, exchange_type=ExchangeType.fanout
+                )
+                self.queue = self.channel.queue_declare(queue='', exclusive=True)
+                self.channel.queue_bind(
+                    exchange=self.receive_queue_name, queue=self.queue.method.queue
+                )
+                self.channel.basic_consume(
+                    queue=self.queue.method.queue,
+                    on_message_callback=self._on_message_received
+                )
+            else:
+                self.channel.queue_declare(queue=self.receive_queue_name)
+                self.channel.basic_consume(
+                    queue=self.receive_queue_name,
+                    on_message_callback=self._on_message_received
+                )
 
 
     def send_msg(self, message):
         """
-        Sends a message to the configured queue or exchange.
+        Sends a message to the configured queue or exchange with retries.
 
         Args:
             message (str): The message to be sent.
+
+        Raises:
+            Exception: If the message cannot be sent after retries.
         """
-        try:
-            if self.channel.is_open:
+        max_retries = 3
+        retry_delay = 1  # Retraso en segundos entre reintentos
+        attempt = 0
+        sent = False
+
+        while not sent and attempt <= max_retries:
+            try:
+                # Intentar enviar el mensaje
                 if self.publish_is_fanout:
                     self.channel.basic_publish(
-                        exchange=self.publish_queue_name, routing_key='', body=message)
+                        exchange=self.publish_queue_name,
+                        routing_key='',
+                        body=message)
                 else:
                     self.channel.basic_publish(
-                        exchange='', routing_key=self.publish_queue_name, body=message)
-            else:
-                print("El canal está cerrado. No se puede enviar el mensaje.")
-        except pika.exceptions.ChannelClosedByBroker as e:
-            print(f"El canal fue cerrado por el broker: {e}")
-        except pika.exceptions.ChannelWrongStateError as e:
-            print(f"Error de estado del canal: {e}")
-        except Exception as e:
-            print(f"Error inesperado al enviar mensaje: {e}")
+                        exchange='',
+                        routing_key=self.publish_queue_name,
+                        body=message)
+                
+                sent = True
+                self.logger.info(f"Mensaje enviado exitosamente: {message}")
+
+            except pika.exceptions.AMQPConnectionError as e:
+                attempt += 1
+                self.logger.warning(f"Error de conexión al enviar mensaje: {e}. Intento {attempt} de {max_retries}.")
+
+                if attempt <= max_retries:
+                    self.logger.info(f"Reintentando en {retry_delay} segundos...")
+                    time.sleep(retry_delay)
+                    self.__reconnect()  # Intentar reconectar antes del próximo intento
+                else:
+                    self.logger.error(f"No se pudo enviar el mensaje después de {max_retries} intentos.")
+                    raise Exception("Fallo al enviar el mensaje después de múltiples intentos") from e
+
+
 
 
     def _on_message_received(
@@ -141,17 +185,7 @@ class MsgDispatcher:
             body (bytes): The body of the received message.
         """
         
-        print(f"Mensaje recibido: {body}")
-        
-
-        if self.stop_consuming_after_received_msg:
-            # This is needed when the program has to do something else after handling the message.
-            # This is the case for the parking main, where it needs to do more more work after receiving and processing the message.
-            # Another solution would be to create another thread for this, but this solution hasnt been studied yet.
-            ch.stop_consuming() 
-
-        ch.basic_ack(delivery_tag=method.delivery_tag)
-
+        self.logger.info(f"Mensaje recibido: {body}")
         # Every program that uses this type of object may have a different way of handling messages.
         # E.g. The parking waits and handles a different message compared to the gate program.
         # The result is assigned to the atribute, to get it later, and more work with it.
@@ -160,6 +194,16 @@ class MsgDispatcher:
 
         if self.reply_to_received_msg:
             self.send_msg(self.last_reply_result)
+
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+        self.logger.debug("Mensaje procesado y reconocido.")
+
+        if self.stop_consuming_after_received_msg:
+            # This is needed when the program has to do something else after handling the message.
+            # This is the case for the parking main, where it needs to do more more work after receiving and processing the message.
+            # Another solution would be to create another thread for this, but this solution hasnt been studied yet.
+            ch.stop_consuming()
+            self.logger.info("Consumo detenido tras procesar el mensaje.")
 
     
     def get_reply_result(self):
@@ -171,24 +215,36 @@ class MsgDispatcher:
             Any: The result from the last execution of the msg_handler.
         """
         return self.last_reply_result
+
         
 
     def wait_and_receive_msg(self):
         """
         Starts waiting to receive messages.
         """
-        try:
-            if self.channel.is_open:
-                self.channel.start_consuming()
-            else:
-                print("El canal está cerrado. No se puede consumir mensajes.")
-        except pika.exceptions.ConnectionClosedByBroker as e:
-            print(f"La conexión fue cerrada por el broker: {e}")
-        except pika.exceptions.ChannelWrongStateError as e:
-            print(f"Error de estado del canal al consumir mensajes: {e}")
-        except Exception as e:
-            print(f"Error inesperado durante la recepción de mensajes: {e}")
-            self.close()
+        max_retries = 3
+        retry_delay = 1  # Retraso en segundos entre reintentos
+        attempt = 0
+        sent = False
+
+        while not sent and attempt <= max_retries:
+            try:
+                if self.channel.is_open:
+                    self.logger.info("Esperando mensajes...")
+                    self.channel.start_consuming()
+                else:
+                    self.logger.warning("El canal está cerrado. No se puede consumir mensajes.")
+            except pika.exceptions.AMQPConnectionError as e:
+                    attempt += 1
+                    self.logger.warning(f"Error de conexión al enviar mensaje: {e}. Intento {attempt} de {max_retries}.")
+
+                    if attempt <= max_retries:
+                        self.logger.info(f"Reintentando en {retry_delay} segundos...")
+                        time.sleep(retry_delay)
+                        self.__reconnect()  # Intentar reconectar antes del próximo intento
+                    else:
+                        self.logger.error(f"No se pudo enviar el mensaje después de {max_retries} intentos.")
+                        raise Exception("Fallo al enviar el mensaje después de múltiples intentos") from e
 
     def close(self):
         """
@@ -196,12 +252,9 @@ class MsgDispatcher:
         Closes the connection to RabbitMQ.
 
         """
-        try:
-            if self.connection and self.connection.is_open:
-                self.connection.close()
-            else:
-                print("La conexión ya estaba cerrada o es inválida.")
-        except pika.exceptions.ConnectionWrongStateError as e:
-            print(f"Error de estado de la conexión al cerrar: {e}")
-        except Exception as e:
-            print(f"Error inesperado al cerrar la conexión: {e}")
+        self.logger.info("Intentando cerrar conexión...")
+        if self.connection and self.connection.is_open:
+            self.connection.close()
+            self.logger.info("Conexion cerrada.")
+        else:
+            self.logger.info("La conexión ya estaba cerrada o es inválida.")
