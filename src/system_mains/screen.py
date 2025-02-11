b8a5ca5bc5b4c50503bdf3e6ac2f44f080bbafcf
David Tarca <dt764@inlumine.ual.es>
2025-02-11 19:14:33 +0100

    Modificada la pantalla, codigo optimizado

--- src/system_mains/screen.py
+++ src/system_mains/screen.py
@@ -9,28 +9,30 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from communication.msg_disp_factory import MsgDispatcherFactory
 
 class StatusScreen(QMainWindow):
-    def __init__(self, hostname):
+    def __init__(self):
         super().__init__()
         self.setWindowTitle("Pantalla de Estado del Parking")
         self.status_label = QLabel("Esperando estado...", self)
         self.status_label.setGeometry(50, 50, 300, 50)
-        
-        self.msg_dispatcher = MsgDispatcherFactory.create_screen_dispatcher(hostname)
-
-        threading.Thread(target=self.msg_dispatcher.wait_and_receive_msg, daemon=True).start()
+    
+    def update_status(self, message):
+        self.status_label.setText(message)
 
 
 
 def main():
     app = QApplication(sys.argv)
     hostname = "localhost"
-    screen = StatusScreen(hostname)
+    port=1883
+    screen = StatusScreen()
 
     def handle_message_update(message):
         decoded_message = message.decode()
         screen.update_status(decoded_message)
 
-    screen.msg_dispatcher.msg_handler = handle_message_update
+    msg_dispatcher = MsgDispatcherFactory.create_screen_dispatcher(hostname, port, msg_handler=handle_message_update)
+    threading.Thread(target=msg_dispatcher.wait_and_receive_msg, daemon=True).start()
+    
 
     screen.show()
     sys.exit(app.exec_())
