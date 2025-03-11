31ed0013ab76bd2cbdd7a01c80c8acb9522305d6
David Tarca <dt764@inlumine.ual.es>
2025-03-11 13:01:35 +0100

    Añadido el ajuste por variable de entorno del modo debug

--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,15 @@
 from app import create_app
 from dotenv import load_dotenv
+import os
 import logging
 
-load_dotenv() 
+load_dotenv()
 app = create_app()
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    app.run(debug=True)
+    debug_mode = os.getenv("FLASK_DEBUG", "False").lower() in ("true", "1", "yes")
+    
+    if debug_mode:
+        logging.basicConfig(level=logging.DEBUG)
+    
+    app.run(debug=debug_mode)
