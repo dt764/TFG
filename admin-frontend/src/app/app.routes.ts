38049c54c4dc708fbdf61ebe304fe1e508ea2f81
David Tarca <dt764@inlumine.ual.es>
2025-04-24 23:18:51 +0200

    Modificaciones en el archivo de rutas y de configuración.

--- admin-frontend/src/app/app.routes.ts
+++ admin-frontend/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HistoryComponent } from './history/history.component';
+import { authGuard } from './auth.guard';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    {
+        path: '',
+        component: HomeComponent,
+        canActivate: [authGuard],
+        children: [
+            { path: '', component: HistoryComponent },
+            { path: 'users', loadComponent: () => import('./users/users.component').then(m => m.UsersComponent) },
+            { path: 'add-user', loadComponent: () => import('./add-user/add-user.component').then(m => m.AddUserComponent) },
+            { path: 'users/:id', loadComponent: () => import('./user-detail/user-detail.component').then(m => m.UserDetailComponent) }
+        ]
+    },
+
+    { path: '**', redirectTo: '' },
+];
