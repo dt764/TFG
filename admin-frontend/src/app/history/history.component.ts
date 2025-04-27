b6056b051b7e4dd544782846aa38d377cfe10d98
David Tarca <dt764@inlumine.ual.es>
2025-04-27 02:18:10 +0200

    Añadido filtrado por fecha en componente

--- admin-frontend/src/app/history/history.component.ts
+++ admin-frontend/src/app/history/history.component.ts
@@ -2,12 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { History } from '../interfaces/history';
 import { RouterLink } from '@angular/router';
-import { NgFor } from '@angular/common';
+import { NgClass, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [RouterLink, NgFor],
+  imports: [RouterLink, NgFor, NgClass],
   templateUrl: './history.component.html',
   styleUrl: './history.component.scss',
 })
@@ -15,17 +15,50 @@ import { NgFor } from '@angular/common';
 export class HistoryComponent implements OnInit {
 
   history_entries: History[] = [];
+  filteredEntries: History[] = [];
+  startDate: string = '';
+  endDate: string = '';
 
   constructor(private api: ApiService) {}
 
   loadHistory() {;
     this.api.getHistory().subscribe((data) => {
       this.history_entries = data;
+      this.filteredEntries = [...this.history_entries];
     });
   }
 
   ngOnInit() {
     this.loadHistory();
+    
   } 
 
+  filterByDate(): void {
+    // Si no se ha seleccionado ninguna fecha, mostramos todas las entradas
+    if (!this.startDate && !this.endDate) {
+      this.filteredEntries = [...this.history_entries];
+      return;
+    }
+
+    // Convertimos las fechas de inicio y fin a objetos Date con hora 00:00:00 para startDate y 23:59:59 para endDate
+    const start = this.startDate ? new Date(this.startDate + 'T00:00:00') : null;
+    const end = this.endDate ? new Date(this.endDate + 'T23:59:59') : null;
+
+    this.filteredEntries = this.history_entries.filter(entry => {
+      const entryDate = new Date(entry.date);
+
+      let isInRange = true;
+
+      // Verificamos si la entrada está dentro del rango de fechas
+      if (start && entryDate < start) {
+        isInRange = false;
+      }
+
+      if (end && entryDate > end) {
+        isInRange = false;
+      }
+
+      return isInRange;
+    });
+  }
 }
