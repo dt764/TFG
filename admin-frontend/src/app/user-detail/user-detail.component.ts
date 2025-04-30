2c825c2da01d8586c95f4c584d61bffe5ab79289
David Tarca <dt764@inlumine.ual.es>
2025-04-30 19:46:20 +0200

    Cambios en frontend.

--- admin-frontend/src/app/user-detail/user-detail.component.ts
+++ admin-frontend/src/app/user-detail/user-detail.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { UpdateUser, User } from '../interfaces/User';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from '../api.service';
-import { NgFor, NgIf } from '@angular/common';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { History } from '../interfaces/history';
 import { UpdateUser_FormErrors } from '../interfaces/form_errors';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [NgIf, FormsModule, NgFor],
+  imports: [NgIf, FormsModule, NgFor, CommonModule],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
@@ -25,40 +26,74 @@ export class UserDetailComponent {
   
   successMessage: string | null = null;
 
+  confirmDelete = false;
+
+  startDate: string = '';
+  endDate: string = '';
+  plate_input: any;
+  filteredEntries: History[] = [];
+  activeSection: 'details' | 'history' = 'details';
+
+  isLoading: boolean = false;
   
   constructor(
+    private router: Router,
     private route: ActivatedRoute,
     private apiService: ApiService,
+    private messageService: MessageService  // Inyectar el servicio
   ) {}
 
   ngOnInit(): void {
     this.getUser();
     this.getHistoryUser();
   }
+
+  switchSection(section: 'details' | 'history') {
+    this.activeSection = section;
+  }
   
   getUser(): void {
     const id = String(this.route.snapshot.paramMap.get('id'));
-    this.apiService.getUser(id)
-      .subscribe(User => {
+    this.isLoading = true;
+    this.apiService.getUser(id).subscribe({
+      next: User => {
         this.user = User;
-      });
+        this.isLoading = false;
+      },
+      error: () => {
+        this.isLoading = false;
+      }
+    });
   }
-
+  
   getHistoryUser(): void {
     const id = String(this.route.snapshot.paramMap.get('id'));
-    this.apiService.getUserHistory(id)
-      .subscribe(userHistory => {
+    this.isLoading = true;
+    this.apiService.getUserHistory(id).subscribe({
+      next: userHistory => {
         this.userHistory = userHistory;
-      });
+        this.filteredEntries = [...this.userHistory];
+        this.isLoading = false;
+      },
+      error: () => {
+        this.isLoading = false;
+      }
+    });
   }
+
 
   save(): void {
     if (this.user && this.user.id !== undefined) {
+      const cleanedPlates = this.user.plates
+      .map(plate => plate.trim()) // eliminar espacios
+      .filter(plate => plate !== ''); // eliminar vacíos
+
       this.updateUser = {
         first_name: this.user.first_name,
         last_name: this.user.last_name,
-        plates: this.user.plates
+        plates: cleanedPlates
       };
+
       this.apiService.updateUser(this.updateUser, this.user.id).subscribe({
         next: (user) => {
           this.user = user;
@@ -73,10 +108,53 @@ export class UserDetailComponent {
         error: (err) => {
           if (err.status === 400 && err.error.error) {
             this.formErrors = err.error.error;
-            console.log('Error de validación:', err.error.error);
           }
         }
       });
     }
-  } 
+  }
+
+  deleteUser() {
+    if (this.user){
+      this.apiService.deleteUser(this.user.id).subscribe(() => {
+        this.messageService.showMessage("Usuario eliminado correctamente  ✅.");  // Enviar el mensaje
+        this.confirmDelete = false;
+        this.router.navigate(['/users']);
+      });
+    }
+   
+  }
+  
+  filterEntries(): void {
+    let filtered = [...this.userHistory];
+  
+    // Filtrado por fechas
+    if (this.startDate || this.endDate) {
+      const start = this.startDate ? new Date(this.startDate + 'T00:00:00') : null;
+      const end = this.endDate ? new Date(this.endDate + 'T23:59:59') : null;
+  
+      filtered = filtered.filter(entry => {
+        const entryDate = new Date(entry.date);
+        let isInRange = true;
+  
+        if (start && entryDate < start) {
+          isInRange = false;
+        }
+  
+        if (end && entryDate > end) {
+          isInRange = false;
+        }
+  
+        return isInRange;
+      });
+    }
+  
+    // Filtrado por matrícula
+    if (this.plate_input) {
+      filtered = filtered.filter(entry => entry.plate.includes(this.plate_input));
+    } 
+  
+    this.filteredEntries = filtered;
+  }
+
 }
--- admin-frontend/src/app/users/users.component.ts
+++ admin-frontend/src/app/users/users.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { User } from '../interfaces/User';
 import { ApiService } from '../api.service';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-users',
   standalone: true,
   imports: [
     NgFor,
-    RouterLink
+    RouterLink,
+    NgIf
   ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
@@ -17,11 +19,26 @@ import { RouterLink } from '@angular/router';
 export class UsersComponent {
 
   users: User[] = [];
+  successMessage: string | null = null;
 
-  constructor(private apiService: ApiService) { }
+  constructor(
+    private apiService: ApiService,
+    private messageService: MessageService
+
+  ) { }
 
   ngOnInit(): void {
     this.getUsers();
+
+    // Suscribirse al mensaje de éxito
+    this.messageService.message$.subscribe((message) => {
+      this.successMessage = message;
+      
+      //Eliminar el mensaje después de 3 segundos
+      setTimeout(() => {
+        this.successMessage = null;
+      }, 3000);
+    });
   }
 
   getUsers(): void {
