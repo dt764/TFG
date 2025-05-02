a62bb877a7a382394cf1f0a9c0c48c1df24c355c
David Tarca <dt764@inlumine.ual.es>
2025-05-02 19:09:51 +0200

    Preparado para despliegue en Render.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-echo "Esperando a la base de datos..."
-while ! nc -z db 5432; do
-  sleep 0.5
-done
+#echo "Esperando a la base de datos..."
+#while ! nc -z db 5432; do
+  #sleep 0.5
+#done
 
-echo "Ejecutando flask db upgrade..."
+#echo "Ejecutando flask db upgrade..."
 flask db upgrade
 
-echo "Ejecutando init_app.py..."
+#echo "Ejecutando init_app.py..."
 python init_app.py
 
 echo "Iniciando servidor Flask..."
-exec python run.py
+exec gunicorn run:app
+
